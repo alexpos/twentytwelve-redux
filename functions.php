a4d70228a109bd898010e416eafee2460787e196
Alex Poslavsky <alexander.poslavsky@gmail.com>
2012-12-21 14:54:07 +0100

    Added error checking so the theme can be enabled before adding Redux

--- functions.php
+++ functions.php
@@ -8,5 +8,13 @@ define( 'THEME_URI', get_template_directory_uri() );
 define( 'CHILD_THEME_DIR', get_stylesheet_directory() );
 define( 'CHILD_THEME_URI', get_stylesheet_directory_uri() );
 
-define('Redux_OPTIONS_URL', trailingslashit( CHILD_THEME_URI ) . 'admin/options/' );
-require_once( trailingslashit( CHILD_THEME_DIR ) . 'admin/options.php');
+if (is_dir(trailingslashit( CHILD_THEME_DIR ) . 'admin/')) {
+  define('Redux_OPTIONS_URL', trailingslashit( CHILD_THEME_URI ) . 'admin/options/' );
+  require_once( trailingslashit( CHILD_THEME_DIR ) . 'admin/options.php');
+} else {
+  add_action('admin_notices', 'redux_install__admin_notice');
+}
+
+function redux_install__admin_notice(){
+    echo '<div class="error"> <p>Add the Redux admin folder as explained in the <a href="#">readme file!</a></p> </div>';
+}
